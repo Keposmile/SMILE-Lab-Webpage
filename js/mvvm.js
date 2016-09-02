4bc75a56bb81c4e9593f4e229563921d7794471d
xy z <xy z>
2016-09-02 10:52:56 +0800

    添加主题keyword选择控制，label标签变色

--- js/mvvm.js
+++ js/mvvm.js
@@ -4,16 +4,20 @@ $(function(){
     $id:"table-columns",
     head:[{
         title:"大众汽车尾气排放丑闻",
-        groupId:1
+        groupId:1,
+        disabled:false
       },{
         title:"美国总统选举",
-        groupId:2
+        groupId:2,
+        disabled:false
       },{
         title:"叙利亚战争",
-        groupId:3
+        groupId:3,
+        disabled:false
       },{
         title:"伊斯兰国",
-        groupId:4
+        groupId:4,
+        disabled:false
       }
     ],
     keywords_1:{
@@ -46,6 +50,7 @@ $(function(){
       festival:"festival"
     },
     keywords_4:{
+
       team:"team",
       enterprise:"enterprise",
       league:"league",
@@ -59,16 +64,44 @@ $(function(){
       column3:[],
       column4:[],
     },
+    disabled_other_columns:function(e){
+      //禁用其他列的方法
+      var _this=$(e.target);
+      if(_this.find("input[type=checkbox]:eq(0)").attr("disabled")){
+        console.log("out");
+        return false;
+      }
+
+      // label样式设置
+      var _thisColumnClass=$(e.target).attr("class");
+      if(_this.attr("data-selected")=="selected"){
+        _this.attr("data-selected","");
+      }else {
+        _this.attr("data-selected","selected");
+      }
+
+      // console.log(_thisColumnClass);
+      var pattern=/(keywords_)([0-9])+/;
+      var _thisGroupIndex=_thisColumnClass.match(pattern)[2];
+      // console.log(_thisGroupIndex);
+      var otherColumnInput=$("#hide-tab-1 table input[type=checkbox]:not(."+_thisColumnClass+")");//获取非本列的input checkbox
+      otherColumnInput.attr("disabled",true).parent().addClass("checkbox-disabled");//禁用其他列
+      // console.log($("#hide-tab-1 table input[type=checkbox]:not(."+_thisColumnClass+")"));
+      if(this.selected["column"+_thisGroupIndex].length===0){//检测到本列数据为空时，还原其他列
+        otherColumnInput.attr("disabled",false).parent().removeClass("checkbox-disabled");
+      }
+      console.log($("#hide-tab-1 table input[type=checkbox][checked=true]").parent().length);
+    },
     remove_from_table_selected:function(e){
       var thisKeyword=$(e.target).parent().parent().find("span:eq(1)").text();
       console.log($("#"+thisKeyword));
-      $("#"+thisKeyword).trigger("click");
+      $("#"+thisKeyword).trigger("click").parent().attr("data-selected","");
     },
     remove_all_selected:function(){
       var selected= this.selected;
       for (var i in selected) {
         for(var j=0;selected[i].length!==0;){
-          $("#"+selected[i][j]).trigger("click");
+          $("#"+selected[i][j]).trigger("click").parent().attr("data-selected","");
         }
       }
     },
@@ -94,7 +127,7 @@ $(function(){
 function setUp_keyword_table_column(columnNum){
   $("#hide-tab-1 table>thead>tr").append("<th ms-for=\"(key,el) in @head | limitBy("+columnNum+")\" ms-attr=\"{id:@head[key].groupId}\">{{el.title}}</th>");
   for (var i = 1; i <= columnNum; i++) {
-    $("#hide-tab-1 table>tbody>tr").append("<td><label ms-for=\"(key,el) in @keywords_"+i+"\" onselectstart=\"return false\" ms-attr=\"{for:@keywords_"+i+"[key]}\"><input  type=\"checkbox\" ms-duplex=\"@selected.column"+i+"\" ms-attr=\"{id:@keywords_"+i+"[key],value:@keywords_"+i+"[key],class:'keywords_"+i+"'}\">{{el}}</label><!-- <div>{{@selected.column1}}</div> --></td>");
+    $("#hide-tab-1 table>tbody>tr").append("<td><label ms-for=\"(key,el) in @keywords_"+i+"\" onselectstart=\"return false\"  ms-on-click=\"@disabled_other_columns\"  ms-attr=\"{for:@keywords_"+i+"[key],class:'keywords_"+i+"'}\"><input  type=\"checkbox\" ms-duplex=\"@selected.column"+i+"\" ms-attr=\"{id:@keywords_"+i+"[key],value:@keywords_"+i+"[key],class:'keywords_"+i+"'}\">{{el}}</label><!-- <div>{{@selected.column1}}</div> --></td>");
   }
 }
 function isEmpty(obj)
